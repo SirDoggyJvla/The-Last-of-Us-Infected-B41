fb540cf3720c90d5b2f47d87467526ecb61a241d
SirDoggyJvla <106160594+SirDoggyJvla@users.noreply.github.com>
2024-02-14 23:22:54 +0100

    Initial commit

---
+++ Contents/mods/The Last of Us Infected/media/lua/shared/TLOU_utility.lua
@@ -0,0 +1,20 @@
+
+--[[ ================================================ ]]--
+--[[  /~~\'      |~~\                  ~~|~    |      ]]--
+--[[  '--.||/~\  |   |/~\/~~|/~~|\  /    | \  /|/~~|  ]]--
+--[[  \__/||     |__/ \_/\__|\__| \/   \_|  \/ |\__|  ]]--
+--[[                     \__|\__|_/                   ]]--
+--[[ ================================================ ]]--
+--[[
+
+This file defines the core of the mod of The Last of Us Infected Fork
+
+]]--
+--[[ ================================================ ]]--
+
+-- main TLOU Infected module for use in storing informations and pass along other files
+local TLOU_infected = {}
+
+
+
+return TLOU_infected
